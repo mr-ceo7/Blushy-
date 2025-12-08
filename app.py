bb76d5a1f1ce1e08892fb057543a216adfa33ccd
mr-ceo7 <kassimmusa322@gmail.com>
2025-12-09 00:30:20 +0300

    feat: connect to render postgresql

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ def create_app():
     app = Flask(__name__)
     
     # Configuration
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-        'DATABASE_URL', 
-        'sqlite:///blushy.db'
-    )
+    database_url = os.environ.get('DATABASE_URL', 'sqlite:///blushy.db')
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+    
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JSON_SORT_KEYS'] = False
     
